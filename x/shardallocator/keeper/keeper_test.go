496f0bb741d0d899bfd173faeb61eac0744b5eb5
yueijdguedh <yueijdguedh@gmail.com>
2025-01-08 14:30:00 +0800

    test: add node registration tests

---
+++ x/shardallocator/keeper/keeper_test.go
@@ -0,0 +1,11 @@
+package keeper_test
+
+import (
+	"testing"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNodeRegistration(t *testing.T) {
+	// TODO: Implement test
+	require.True(t, true)
+}
