5ece54941edac4b5683048f1cb75ec31c1510483
yueijdguedh <yueijdguedh@gmail.com>
2025-07-13 00:00:00 +0800

    feat: add reward genesis handler

---
+++ x/reward/keeper/genesis.go
@@ -0,0 +1,13 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/yueijdguedh/ReflexNet/x/reward/types"
+)
+
+func (k Keeper) InitGenesis(ctx sdk.Context, gs types.GenesisState) {
+}
+
+func (k Keeper) ExportGenesis(ctx sdk.Context) *types.GenesisState {
+	return types.DefaultGenesis()
+}
