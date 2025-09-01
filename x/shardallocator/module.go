42602b2072f731d226a4e0093a25796ddf66ca30
JacksonJackson-1np <aapwjbthqo@rambler.ru>
2025-09-02 00:00:00 +0800

    feat: add shardallocator module

---
+++ x/shardallocator/module.go
@@ -0,0 +1,15 @@
+package shardallocator
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/yueijdguedh/ReflexNet/x/shardallocator/keeper"
+)
+
+type AppModule struct {
+	keeper keeper.Keeper
+}
+
+func NewAppModule(cdc codec.Codec, keeper keeper.Keeper) AppModule {
+	return AppModule{keeper: keeper}
+}
