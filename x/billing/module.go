078d04fad1576af21275bdb48e81c790a75e096b
MasonWhite-69a <utgpdcxnzy@rambler.ru>
2025-09-08 00:00:00 +0800

    feat: add billing module

---
+++ x/billing/module.go
@@ -0,0 +1,15 @@
+package billing
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/yueijdguedh/ReflexNet/x/billing/keeper"
+)
+
+type AppModule struct {
+	keeper keeper.Keeper
+}
+
+func NewAppModule(cdc codec.Codec, keeper keeper.Keeper) AppModule {
+	return AppModule{keeper: keeper}
+}
